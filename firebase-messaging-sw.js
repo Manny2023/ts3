8d7cd81566c1da0b1997a2aa3861538241d8a51a
Manny2023 <120243119+Manny2023@users.noreply.github.com>
2024-10-20 12:31:06 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,31 +1,26 @@
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js'
+  // Agrega más rutas según necesites
+];
 
-// Configuración de Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyA4TDdmsxfthFR4PTuBAhEQdutP06i9c2I",
-    authDomain: "telesecundaria-ts.firebaseapp.com",
-    projectId: "telesecundaria-ts",
-    storageBucket: "telesecundaria-ts.appspot.com",
-    messagingSenderId: "572048540410",
-    appId: "1:572048540410:web:e08e9c3971b191e92cbf43",
-    measurementId: "G-T6V43KVHRX"
-};
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-// Manejar mensajes en segundo plano
-messaging.onBackgroundMessage((payload) => {
-    console.log('Mensaje recibido en segundo plano: ', payload);
-
-    const notificationTitle = payload.notification.title || 'Título por defecto';
-    const notificationOptions = {
-        body: payload.notification.body || 'Cuerpo de notificación por defecto',
-        icon: payload.notification.icon || 'https://i.ibb.co/T1jVqPw/favicon-32x32.png',
-    };
-
-    // Mostrar la notificación
-    self.registration.showNotification(notificationTitle, notificationOptions);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
